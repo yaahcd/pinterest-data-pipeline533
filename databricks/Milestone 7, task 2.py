674988cfca5eb9125423cf50a17f0c3be31f1e4c
yaahcd <yaahcd>
2024-07-25 08:56:44 +0000

    adds code for milestone 7, task 2

---
+++ databricks/Milestone 7, task 2.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %run "./Milestone 6, task 2"
+
+# COMMAND ----------
+
+from pyspark.sql.functions import concat, lit, to_timestamp
+
+df_geo = df_geo.replace({'User Info Error': None})
+df_geo = df_geo.withColumn("coordinates", concat("latitude", lit(" , "), "longitude"))
+df_geo = df_geo.drop("latitude", "longitude")
+df_geo = df_geo.withColumn("timestamp", to_timestamp("timestamp"))
+df_geo = df_geo.select("ind", "country", "coordinates", "timestamp")
