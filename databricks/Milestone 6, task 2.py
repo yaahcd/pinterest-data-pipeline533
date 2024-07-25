1adf762e968e756535fefe36bdd83d840a09cf6e
yaahcd <yaahcd>
2024-07-25 08:01:13 +0000

    Updates milestone 06, task 02 code to remove mount

--- databricks/Milestone 6, task 2.py
+++ databricks/Milestone 6, task 2.py
@@ -1,26 +1,4 @@
 # Databricks notebook source
-from pyspark.sql.functions import *
-import urllib
-
-delta_table_path = "dbfs:/user/hive/warehouse/authentication_credentials"
-
-aws_keys_df = spark.read.format("delta").load(delta_table_path)
-
-ACCESS_KEY = aws_keys_df.select('Access key ID').collect()[0]['Access key ID']
-SECRET_KEY = aws_keys_df.select('Secret access key').collect()[0]['Secret access key']
-
-ENCODED_SECRET_KEY = urllib.parse.quote(string=SECRET_KEY, safe="")
-
-AWS_S3_BUCKET = "user-0e03d1c30c91-bucket"
-
-MOUNT_NAME = "/mnt/pinterest_data"
-
-SOURCE_URL = "s3n://{0}:{1}@{2}".format(ACCESS_KEY, ENCODED_SECRET_KEY, AWS_S3_BUCKET)
-
-dbutils.fs.mount(SOURCE_URL, MOUNT_NAME)
-
-# COMMAND ----------
-
 # MAGIC %sql
 # MAGIC
 # MAGIC SET spark.databricks.delta.formatCheck.enabled=false
